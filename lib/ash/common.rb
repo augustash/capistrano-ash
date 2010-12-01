c7282145e197c328c98d26b917c6f87ec6f97855
Peter McWilliams <petemcw@gmail.com>
2010-12-01 10:47:40 -0600

    Added basic documentation for function.

--- lib/ash/common.rb
+++ lib/ash/common.rb
@@ -1,3 +1,8 @@
+# Accepts a parameter name and value and registers it as a default within Capistrano.
+# Params:
+# +name+
+# +args+
+# +block+
 def _cset(name, *args, &block)
   unless exists?(name)
     set(name, *args, &block)
