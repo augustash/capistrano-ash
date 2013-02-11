eac89bdc6ccf826089058f0df59687e03b483a9f
Josh Johnson <josh@augustash.com>
2013-02-11 10:36:19 -0600

    add namespace and tasks for compiling compass/sass stylesheets and uploading them to the remote server

--- lib/ash/base.rb
+++ lib/ash/base.rb
@@ -85,6 +85,9 @@ configuration.load do
   set :db_regex_hash, {
   }
 
+
+
+
   # --------------------------------------------
   # Overloaded tasks
   # --------------------------------------------
@@ -426,6 +429,106 @@ configuration.load do
       end
     end
   end
+
+  # --------------------------------------------
+  # Compass/Sass compiling
+  # --------------------------------------------
+  namespace :compass do
+
+    desc "Compile SASS stylesheets and upload to remote server"
+    task :default do
+      compile
+      upload_stylesheets
+      ash.fixperms
+    end
+
+    desc 'Uploads compiled stylesheets to their matching watched directories'
+    task :upload_stylesheets, :roles => :web, :except => { :no_release => true } do
+      watched_dirs          = fetch(:compass_watched_dirs, nil)
+      stylesheets_dir_name  = fetch(:stylesheets_dir_name, 'stylesheets')
+
+      if !watched_dirs.nil?
+        if watched_dirs.is_a? String
+          logger.debug "Uploading compiled stylesheets for #{watched_dirs}"
+          logger.debug "trying to upload stylesheets from ./#{watched_dirs}/#{stylesheets_dir_name} -> #{latest_release}/#{watched_dirs}/#{stylesheets_dir_name}"
+
+          upload_command = "scp -r ./#{watched_dirs}/#{stylesheets_dir_name}/*.css #{user}@#{application}:#{latest_release}/#{watched_dirs}/#{stylesheets_dir_name}/"
+
+          logger.info "running SCP command:"
+          logger.debug upload_command
+          system(upload_command)
+
+          # upload("./#{watched_dirs}/stylesheets/global-crest.css", "#{latest_release}/#{watched_dirs}/stylesheets/", :via => :scp)
+        elsif watched_dirs.is_a? Array
+          logger.debug "Uploading compiled stylesheets for #{watched_dirs.join(', ')}"
+          watched_dirs.each do |dir|
+            logger.debug "trying to upload stylesheets from ./#{dir}/#{stylesheets_dir_name}/ -> #{latest_release}/#{dir}/#{stylesheets_dir_name}/"
+
+            upload_command = "scp -r ./#{dir}/#{stylesheets_dir_name}/*.css #{user}@#{application}:#{latest_release}/#{dir}/#{stylesheets_dir_name}/"
+
+            logger.info "running SCP command:"
+            logger.debug upload_command
+            system(upload_command)
+          end
+        else
+          logger.debug "Unable to upload compiled stylesheets because :watched_dirs was neither a String nor an Array"
+        end
+      else
+        logger.info "Skipping uploading of compiled stylesheets `compass:upload` because `:watched_dirs` wasn't set"
+      end
+    end
+
+    desc 'Compile minified version of CSS assets using Compass gem'
+    task :compile, :roles => :web, :except => { :no_release => true } do
+      watched_dirs = fetch(:compass_watched_dirs, nil)
+
+      if !watched_dirs.nil?
+        compass_bin_local     = find_compass_bin_path
+        compass_bin           = fetch(:compass_bin, compass_bin_local)
+        compass_env           = fetch(:compass_env, "production")
+        compass_output        = fetch(:compass_output, 'compressed') # nested, expanded, compact, compressed
+
+        if !compass_bin.nil?
+          if watched_dirs.is_a? String
+            logger.debug "Compiling SASS for #{watched_dirs}"
+            system "#{compass_bin} compile --output-style #{compass_output} --environment #{compass_env} ./#{watched_dirs}"
+          elsif watched_dirs.is_a? Array
+            logger.debug "Compiling SASS for #{watched_dirs.join(', ')}"
+            watched_dirs.each do |dir|
+              system "#{compass_bin} compile --output-style #{compass_output} --environment #{compass_env} ./#{dir}"
+            end
+          else
+            logger.debug "Unable to compile SASS because :compass_watched_dirs was neither a String nor an Array"
+          end
+        else
+          logger.info "Skipping SASS compilation in `compass:compile` because unable to find the bin executable for the compass gem"
+        end
+      else
+        logger.info "Skipping compass Sass compiliation"
+      end
+    end
+
+    desc "Finds the bin executable path for the compass gem"
+    task :find_compass_bin_path, :except => { :no_release => true } do
+      begin
+        spec      = Gem::Specification.find_by_name("compass")
+        gem_root  = spec.gem_dir
+        gem_bin   = gem_root + "/bin/compass"
+      rescue Gem::LoadError => e
+        logger.debug "Unable to find the gem 'compass'! Check to see if it's installed: `gem list -d compass` or install: `gem install compass`"
+        gem_bin = nil
+      rescue Exception => e
+        logger.debug "Unable to find the compass executable bin path because of this error: #{e.message}"
+        gem_bin = nil
+      end
+
+      logger.debug "Path to compass executable: #{gem_bin.inspect}"
+
+      # return the path the compass executable
+      gem_bin
+    end
+  end
+
 
   # --------------------------------------------
   # Remote File/Directory test tasks
