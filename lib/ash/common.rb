22094b3b25e6f1d2d48139bf66ff582c3e1d8835
Josh Johnson <josh@augustash.com>
2014-01-12 23:17:21 -0600

    Break xargs list of variables into chunks

--- lib/ash/common.rb
+++ lib/ash/common.rb
@@ -45,7 +45,7 @@ end
 # set the permissions for files recurisvely from the starting directory (dir_path)
 def set_perms_files(dir_path, perm = 644)
   begin
-    run "find #{dir_path} -type f -print0 | xargs -0 #{try_sudo} chmod #{perm}" if remote_dir_exists?(dir_path)
+    run "find #{dir_path} -type f -print0 | xargs -0 -L 500 #{try_sudo} chmod #{perm}" if remote_dir_exists?(dir_path)
   rescue Exception => e
     logger.important "FAILED to set permissions of #{perm} on files within #{dir_path}!"
     logger.important e.message
@@ -62,7 +62,7 @@ end
 # set the permissions for directories recursively from the starting directory (dir_path)
 def set_perms_dirs(dir_path, perm = 755)
   begin
-    run "find #{dir_path} -type d -print0 | xargs -0 #{try_sudo} chmod #{perm}" if remote_dir_exists?(dir_path)
+    run "find #{dir_path} -type d -print0 | xargs -0 -L 500 #{try_sudo} chmod #{perm}" if remote_dir_exists?(dir_path)
   rescue Exception => e
     logger.important "FAILED to set permissions of #{perm} on directories within #{dir_path}!"
     logger.important e.message
